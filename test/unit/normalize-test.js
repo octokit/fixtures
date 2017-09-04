46f1ec6a823b5f7571d99f2e66e8247423e52d42
Gregor <gregor@martynus.net>
2017-09-04 16:15:02 -0700

    test: initial version

---
+++ test/unit/normalize-test.js
@@ -0,0 +1,18 @@
+const {diffString} = require('json-diff')
+const {test} = require('tap')
+
+const normalize = require('../../lib/normalize')
+
+const fixtures = {
+  getRepository: {
+    in: require('../fixtures/get-repository-raw.json'),
+    out: require('../../fixtures/api.github.com/get-repository.json')[0]
+  }
+}
+
+test('normalize', (t) => {
+  const actual = normalize(fixtures.getRepository.in)
+  const expected = fixtures.getRepository.out
+  t.deepEqual(actual, expected, diffString(actual, expected))
+  t.end()
+})
