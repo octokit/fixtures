eb1e81eee5c9106edfefabb98e6862672acbbe0f
Gregor <gregor@martynus.net>
2017-09-04 17:31:57 -0700

    feat: initial version

---
+++ bin/serve.js
@@ -0,0 +1,38 @@
+#!/usr/bin/env node
+
+const express = require('express')
+const glob = require('glob')
+const nock = require('nock')
+const proxy = require('http-proxy-middleware')
+
+const fixturePaths = glob.sync('fixtures/api.github.com/*.json')
+fixturePaths.map(nock.load).forEach((fixtureMocks) => {
+  // by default, nock only allows each mocked route to be called once, afterwards
+  // it returns a "No match for request" error. mock.persist() works around that
+  fixtureMocks.forEach(mock => mock.persist())
+})
+
+const app = express()
+app.use('/', proxy({
+  target: 'https://api.github.com',
+  changeOrigin: true,
+  loglevel: 'debug',
+  onError (error, request, response) {
+    response.writeHead(500, {
+      'Content-Type': 'application/json; charset=utf-8'
+    })
+
+    if (error.message.indexOf('Nock: No match for request') !== 0) {
+      return response.end(error.message)
+    }
+
+    const errorRequestJson = JSON.parse(error.message.substr('Nock: No match for request '.length))
+
+    response.end(JSON.stringify({
+      error: 'Nock: No match for request',
+      request: errorRequestJson
+    }, null, 2) + '\n')
+  }
+}))
+app.listen(3000)
+console.log('🌐  http://localhost:3000')
