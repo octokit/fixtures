936618da0a06490919dee37457740cd139df87aa
Gregor <gregor@martynus.net>
2017-09-17 19:45:25 -0700

    feat(scenario): add and remove repository collaborator

---
+++ scenarios/api.github.com/add-and-remove-repository-collaborator.js
@@ -0,0 +1,71 @@
+module.exports = addAndRemoveRepostioryCollaborator
+
+const env = require('../../lib/env')
+
+// - As user A, invite user B as collaborator to repository "octokit-fixture-org/hello-world"
+// - As user A, list invitations
+// - As user B, accept invitation
+// - As user A, list collaborators (now includes user B)
+// - As user A, remove user B as collaborator from repository
+// - As user A, list collaborators (no longer includes user B)
+async function addAndRemoveRepostioryCollaborator (state) {
+  // https://developer.github.com/v3/repos/collaborators/#add-user-as-a-collaborator
+  await state.request({
+    method: 'put',
+    url: '/repos/octokit-fixture-org/hello-world/collaborators/octokit-fixture-user-b',
+    headers: {
+      Accept: 'application/vnd.github.v3+json',
+      Authorization: `token ${env.FIXTURES_USER_A_TOKEN_FULL_ACCESS}`
+    }
+  })
+
+  // https://developer.github.com/v3/repos/invitations/
+  const invitationsResponse = await state.request({
+    method: 'get',
+    url: '/repos/octokit-fixture-org/hello-world/invitations',
+    headers: {
+      Accept: 'application/vnd.github.v3+json',
+      Authorization: `token ${env.FIXTURES_USER_A_TOKEN_FULL_ACCESS}`
+    }
+  })
+
+  // https://developer.github.com/v3/repos/invitations/#accept-a-repository-invitation
+  await state.request({
+    method: 'patch',
+    url: `/user/repository_invitations/${invitationsResponse.data[0].id}`,
+    headers: {
+      Accept: 'application/vnd.github.v3+json',
+      Authorization: `token ${env.FIXTURES_USER_B_TOKEN_FULL_ACCESS}`
+    }
+  })
+
+  // https://developer.github.com/v3/repos/collaborators/#list-collaborators
+  await state.request({
+    method: 'get',
+    url: '/repos/octokit-fixture-org/hello-world/collaborators',
+    headers: {
+      Accept: 'application/vnd.github.v3+json',
+      Authorization: `token ${env.FIXTURES_USER_A_TOKEN_FULL_ACCESS}`
+    }
+  })
+
+  // https://developer.github.com/v3/repos/collaborators/#remove-user-as-a-collaborator
+  await state.request({
+    method: 'delete',
+    url: '/repos/octokit-fixture-org/hello-world/collaborators/octokit-fixture-user-b',
+    headers: {
+      Accept: 'application/vnd.github.v3+json',
+      Authorization: `token ${env.FIXTURES_USER_A_TOKEN_FULL_ACCESS}`
+    }
+  })
+
+  // https://developer.github.com/v3/repos/collaborators/#list-collaborators
+  await state.request({
+    method: 'get',
+    url: '/repos/octokit-fixture-org/hello-world/collaborators',
+    headers: {
+      Accept: 'application/vnd.github.v3+json',
+      Authorization: `token ${env.FIXTURES_USER_A_TOKEN_FULL_ACCESS}`
+    }
+  })
+}
