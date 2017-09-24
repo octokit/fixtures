f301c1efd0700270a8d62b082d63da6cfdb47912
Gregor <gregor@martynus.net>
2017-09-23 21:41:14 -0700

    refactor: rename variable

--- test/unit/normalize-repository-test.js
+++ test/unit/normalize-repository-test.js
@@ -6,18 +6,18 @@ const organizationRepositoryResponse = require('../fixtures/get-organization-rep
 const userRepositoryResponse = require('../fixtures/get-user-repository.json').response
 
 test('normalize organization repository', (t) => {
-  const fixture = Object.assign({}, organizationRepositoryResponse)
-  normalizeRepository(fixture)
+  const response = Object.assign({}, organizationRepositoryResponse)
+  normalizeRepository(response)
 
-  t.is(fixture.id, 1, 'sets id to 1')
-  t.is(fixture.owner.id, 1, 'sets owner.id to 1')
+  t.is(response.id, 1, 'sets id to 1')
+  t.is(response.owner.id, 1, 'sets owner.id to 1')
   t.end()
 })
 
 test('normalize user repository', (t) => {
-  const fixture = Object.assign({}, userRepositoryResponse)
-  normalizeRepository(fixture)
+  const response = Object.assign({}, userRepositoryResponse)
+  normalizeRepository(response)
 
-  t.is(fixture.owner.id, 1, 'sets owner.id to 1')
+  t.is(response.owner.id, 1, 'sets owner.id to 1')
   t.end()
 })
