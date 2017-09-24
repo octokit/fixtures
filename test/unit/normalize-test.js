890b0c0b4a5259bf2c0e2fd08fca8a831a8a1900
Gregor <gregor@martynus.net>
2017-09-23 21:44:09 -0700

    test: temporary repositories

--- test/unit/normalize-test.js
+++ test/unit/normalize-test.js
@@ -8,9 +8,8 @@ const fixtures = {
     in: require('../fixtures/get-organization-repository.json'),
     out: require('../../fixtures/api.github.com/get-repository.json')[0]
   },
-  getRootViaNowProxy: {
-    in: require('../fixtures/get-root-via-now-proxy.json')
-  }
+  getRootViaNowProxy: require('../fixtures/get-root-via-now-proxy.json'),
+  getTemporaryRepository: require('../fixtures/get-temporary-repository.json')
 }
 
 test('normalize', (t) => {
@@ -22,9 +21,17 @@ test('normalize', (t) => {
 })
 
 test('normalize now proxy response #20', (t) => {
-  const response = normalize(fixtures.getRootViaNowProxy.in)
+  const response = normalize(fixtures.getRootViaNowProxy)
 
   t.is(response.headers['x-now-region'], undefined)
+
+  t.end()
+})
+
+test('normalize request paths containing temporary repository name #23', (t) => {
+  const fixture = normalize(fixtures.getTemporaryRepository)
+
+  t.is(fixture.path, '/repos/octokit-fixture-org/bar')
 
   t.end()
 })
