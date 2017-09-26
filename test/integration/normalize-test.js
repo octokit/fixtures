b1017a0325062be8ffa619dec8cbd06c37f14b32
Gregor <gregor@martynus.net>
2017-09-25 21:24:59 -0700

    test: normalization integration tests

---
+++ test/integration/normalize-test.js
@@ -0,0 +1,21 @@
+const glob = require('glob')
+const {test} = require('tap')
+
+const normalize = require('../../lib/normalize')
+
+glob.sync('scenarios/**/raw-fixture.json')
+  .map(path => path.replace(/(^scenarios\/|\/raw-fixture.json$)/g, ''))
+  .forEach(fixturnName => {
+    test(`normalize ${fixturnName}`, t => {
+      const raw = require(`../../scenarios/${fixturnName}/raw-fixture.json`)
+      const expected = require(`../../scenarios/${fixturnName}/normalized-fixture.json`)
+
+      const actual = raw.filter(isntIgnored).map(normalize)
+      t.deepEqual(actual, expected)
+      t.end()
+    })
+  })
+
+function isntIgnored (fixture) {
+  return !fixture.reqheaders['x-octokit-fixture-ignore']
+}
