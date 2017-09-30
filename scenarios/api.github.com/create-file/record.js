2c98adc75b4efa438d7d1f17adbb8b3ef314e637
Gregor <gregor@martynus.net>
2017-09-30 16:47:52 -0700

    feat(scenario): create file

---
+++ scenarios/api.github.com/create-file/record.js
@@ -0,0 +1,41 @@
+module.exports = createFile
+
+const env = require('../../../lib/env')
+const getTemporaryRepository = require('../../../lib/temporary-repository')
+
+async function createFile (state) {
+  let error
+  // create a temporary repository
+  const temporaryRepository = getTemporaryRepository({
+    request: state.request,
+    token: env.FIXTURES_USER_A_TOKEN_FULL_ACCESS,
+    org: 'octokit-fixture-org',
+    name: 'create-file'
+  })
+
+  await temporaryRepository.create()
+
+  try {
+    // https://developer.github.com/v3/repos/contents/#create-a-file
+    await state.request({
+      method: 'put',
+      url: `/repos/octokit-fixture-org/${temporaryRepository.name}/contents/test.txt`,
+      headers: {
+        Accept: 'application/vnd.github.v3+json',
+        Authorization: `token ${env.FIXTURES_USER_A_TOKEN_FULL_ACCESS}`
+      },
+      data: {
+        message: 'create test.txt',
+        content: Buffer.from('Test content').toString('base64')
+      }
+    })
+  } catch (_error) {
+    error = _error
+  }
+
+  await temporaryRepository.delete()
+
+  if (error) {
+    return Promise.reject(error)
+  }
+}
---
+++ scenarios/api.github.com/create-file/test.js
@@ -0,0 +1,26 @@
+const axios = require('axios')
+const {test} = require('tap')
+
+const fixtures = require('../../..')
+
+test('Get repository', async (t) => {
+  const mock = fixtures.mock('api.github.com/create-file')
+
+  const result = await axios({
+    method: 'put',
+    url: 'https://api.github.com/repos/octokit-fixture-org/create-file/contents/test.txt',
+    headers: {
+      Accept: 'application/vnd.github.v3+json',
+      Authorization: `token 0000000000000000000000000000000000000000`,
+      'Content-Type': 'application/json; charset=utf-8'
+    },
+    data: {
+      message: 'create test.txt',
+      content: Buffer.from('Test content').toString('base64')
+    }
+  }).catch(mock.explain)
+
+  t.doesNotThrow(mock.done.bind(mock), 'satisfies all mocks')
+  t.is(result.data.content.type, 'file')
+  t.end()
+})
