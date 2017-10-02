8b3da585bf735ec26810f373ff22be54af5f2ed0
Gregor <gregor@martynus.net>
2017-10-01 21:45:40 -0700

    feat(scenario): create status

---
+++ scenarios/api.github.com/create-status/record.js
@@ -0,0 +1,93 @@
+module.exports = createStatus
+
+const env = require('../../../lib/env')
+const getTemporaryRepository = require('../../../lib/temporary-repository')
+
+async function createStatus (state) {
+  let error
+  // create a temporary repository
+  const temporaryRepository = getTemporaryRepository({
+    request: state.request,
+    token: env.FIXTURES_USER_A_TOKEN_FULL_ACCESS,
+    org: 'octokit-fixture-org',
+    name: 'create-status'
+  })
+
+  await temporaryRepository.create()
+
+  try {
+    // https://developer.github.com/v3/repos/contents/#create-a-file
+    // (this request gets ignored, we need an existing commit to set status on)
+    const {data: {commit: {sha}}} = await state.request({
+      method: 'put',
+      url: `/repos/octokit-fixture-org/${temporaryRepository.name}/contents/README.md`,
+      headers: {
+        Accept: 'application/vnd.github.v3+json',
+        Authorization: `token ${env.FIXTURES_USER_A_TOKEN_FULL_ACCESS}`,
+        'X-Octokit-Fixture-Ignore': 'true'
+      },
+      data: {
+        message: 'initial commit',
+        content: Buffer.from('# create-status').toString('base64')
+      }
+    })
+
+    // https://developer.github.com/v3/repos/statuses/#create-a-status
+    await state.request({
+      method: 'post',
+      url: `/repos/octokit-fixture-org/${temporaryRepository.name}/statuses/${sha}`,
+      headers: {
+        Accept: 'application/vnd.github.v3+json',
+        Authorization: `token ${env.FIXTURES_USER_A_TOKEN_FULL_ACCESS}`
+      },
+      data: {
+        state: 'failure',
+        target_url: 'https://example.com',
+        description: 'create-status failure test',
+        context: 'example/1'
+      }
+    })
+    await state.request({
+      method: 'post',
+      url: `/repos/octokit-fixture-org/${temporaryRepository.name}/statuses/${sha}`,
+      headers: {
+        Accept: 'application/vnd.github.v3+json',
+        Authorization: `token ${env.FIXTURES_USER_A_TOKEN_FULL_ACCESS}`
+      },
+      data: {
+        state: 'success',
+        target_url: 'https://example.com',
+        description: 'create-status success test',
+        context: 'example/2'
+      }
+    })
+
+    // https://developer.github.com/v3/repos/statuses/#list-statuses-for-a-specific-ref
+    await state.request({
+      method: 'get',
+      url: `/repos/octokit-fixture-org/${temporaryRepository.name}/commits/${sha}/statuses`,
+      headers: {
+        Accept: 'application/vnd.github.v3+json',
+        Authorization: `token ${env.FIXTURES_USER_A_TOKEN_FULL_ACCESS}`
+      }
+    })
+
+    // https://developer.github.com/v3/repos/statuses/#get-the-combined-status-for-a-specific-ref
+    await state.request({
+      method: 'get',
+      url: `/repos/octokit-fixture-org/${temporaryRepository.name}/commits/${sha}/status`,
+      headers: {
+        Accept: 'application/vnd.github.v3+json',
+        Authorization: `token ${env.FIXTURES_USER_A_TOKEN_FULL_ACCESS}`
+      }
+    })
+  } catch (_error) {
+    error = _error
+  }
+
+  await temporaryRepository.delete()
+
+  if (error) {
+    return Promise.reject(error)
+  }
+}
---
+++ scenarios/api.github.com/create-status/test.js
@@ -0,0 +1,66 @@
+const axios = require('axios')
+const {test} = require('tap')
+
+const fixtures = require('../../..')
+
+test('Create status', async (t) => {
+  const mock = fixtures.mock('api.github.com/create-status')
+
+  // create failure status
+  await axios({
+    method: 'post',
+    url: 'https://api.github.com/repos/octokit-fixture-org/create-status/statuses/0000000000000000000000000000000000000000',
+    headers: {
+      Accept: 'application/vnd.github.v3+json',
+      Authorization: `token 0000000000000000000000000000000000000000`,
+      'Content-Type': 'application/json; charset=utf-8'
+    },
+    data: {
+      state: 'failure',
+      target_url: 'https://example.com',
+      description: 'create-status failure test',
+      context: 'example/1'
+    }
+  }).catch(mock.explain)
+
+  // create success status
+  await axios({
+    method: 'post',
+    url: 'https://api.github.com/repos/octokit-fixture-org/create-status/statuses/0000000000000000000000000000000000000000',
+    headers: {
+      Accept: 'application/vnd.github.v3+json',
+      Authorization: `token 0000000000000000000000000000000000000000`,
+      'Content-Type': 'application/json; charset=utf-8'
+    },
+    data: {
+      state: 'success',
+      target_url: 'https://example.com',
+      description: 'create-status success test',
+      context: 'example/2'
+    }
+  }).catch(mock.explain)
+
+  // get all statuses
+  await axios({
+    method: 'get',
+    url: 'https://api.github.com/repos/octokit-fixture-org/create-status/commits/0000000000000000000000000000000000000000/statuses',
+    headers: {
+      Accept: 'application/vnd.github.v3+json',
+      Authorization: `token 0000000000000000000000000000000000000000`
+    }
+  })
+
+  // get combined status
+  const {data} = await axios({
+    method: 'get',
+    url: 'https://api.github.com/repos/octokit-fixture-org/create-status/commits/0000000000000000000000000000000000000000/status',
+    headers: {
+      Accept: 'application/vnd.github.v3+json',
+      Authorization: `token 0000000000000000000000000000000000000000`
+    }
+  })
+
+  t.is(data.state, 'failure', 'combined state is failure')
+  t.doesNotThrow(mock.done.bind(mock), 'satisfies all mocks')
+  t.end()
+})
