e9b04c12380cf7a801a08b11af54cd3e97a9ccbf
Gregor <gregor@martynus.net>
2017-10-17 13:20:02 -0700

    test: get-content test
    copy & paste fail, sorry

--- scenarios/api.github.com/get-content/test.js
+++ scenarios/api.github.com/get-content/test.js
@@ -4,17 +4,18 @@ const {test} = require('tap')
 const fixtures = require('../../..')
 
 test('Get repository', async (t) => {
-  const mock = fixtures.mock('api.github.com/get-repository')
+  const mock = fixtures.mock('api.github.com/get-content')
 
   const result = await axios({
     method: 'get',
-    url: 'https://api.github.com/repos/octokit-fixture-org/hello-world',
+    url: 'https://api.github.com/repos/octokit-fixture-org/hello-world/contents/',
     headers: {
       Accept: 'application/vnd.github.v3+json'
     }
   }).catch(mock.explain)
 
   t.doesNotThrow(mock.done.bind(mock), 'satisfies all mocks')
-  t.is(result.data.id, 1)
+  t.is(result.data.length, 1)
+  t.is(result.data[0].path, 'README.md')
   t.end()
 })
