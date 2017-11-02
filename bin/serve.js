ae9fb6a2a9a0bfeefa0abb6d1406c473798f679b
Gregor <gregor@martynus.net>
2017-11-01 21:07:36 -0700

    fix(server): load fixtures relatively from bin/server.js

--- bin/serve.js
+++ bin/serve.js
@@ -1,11 +1,14 @@
 #!/usr/bin/env node
 
+const pathResolve = require('path').resolve
+
 const express = require('express')
 const glob = require('glob')
 const nock = require('nock')
 const proxy = require('http-proxy-middleware')
 
-const fixturePaths = glob.sync('scenarios/api.github.com/*/normalized-fixture.json')
+const fixturePaths = glob.sync(pathResolve(__dirname, '..', 'scenarios/api.github.com/*/normalized-fixture.json'))
+
 fixturePaths.map(nock.load).forEach((fixtureMocks) => {
   // by default, nock only allows each mocked route to be called once, afterwards
   // it returns a "No match for request" error. mock.persist() works around that
