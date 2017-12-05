3f14926b361bf5c4273e02c8cd52b7cdd0be8175
Martin Smith <martin@mbs3.org>
2017-12-05 12:33:26 -0800

    feat(server): support external fixtures

--- bin/serve.js
+++ bin/serve.js
@@ -6,8 +6,23 @@ const express = require('express')
 const glob = require('glob')
 const nock = require('nock')
 const proxy = require('http-proxy-middleware')
+const yargs = require('yargs')
 
-const fixturePaths = glob.sync(pathResolve(__dirname, '..', 'scenarios/api.github.com/*/normalized-fixture.json'))
+const { argv } = yargs.options({
+  fixtures: {
+    type: 'string',
+    describe: 'Path glob for all test fixtures',
+    default: pathResolve(__dirname, '..', 'scenarios/api.github.com/*/normalized-fixture.json')
+  },
+  loglevel: {
+    type: 'string',
+    describe: 'Set logging level for Express',
+    default: 'debug'
+  }
+}).help()
+
+const fixturePaths = glob.sync(argv.fixtures)
+fixturePaths.forEach(mock => console.log(`Fixture found ${mock}`))
 
 fixturePaths.map(nock.load).forEach((fixtureMocks) => {
   // by default, nock only allows each mocked route to be called once, afterwards
@@ -19,7 +34,7 @@ const app = express()
 app.use('/', proxy({
   target: 'https://api.github.com',
   changeOrigin: true,
-  loglevel: 'debug',
+  loglevel: argv.loglevel,
   onError (error, request, response) {
     response.writeHead(500, {
       'Content-Type': 'application/json; charset=utf-8'
