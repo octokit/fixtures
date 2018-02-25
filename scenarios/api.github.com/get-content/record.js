05580349b70db95a7dd767bf33ce7e6cf1c6dbb0
Gregor <gregor@martynus.net>
2018-02-25 13:31:43 -0800

    feat(scenario): get content with "Accept: application/vnd.github.v3.raw" header

--- scenarios/api.github.com/get-content/record.js
+++ scenarios/api.github.com/get-content/record.js
@@ -1,9 +1,18 @@
 // https://developer.github.com/v3/repos/contents/#get-contents
 // empty path returns README file if present
-module.exports = {
-  method: 'get',
-  url: '/repos/octokit-fixture-org/hello-world/contents/',
-  headers: {
-    Accept: 'application/vnd.github.v3+json'
+module.exports = [
+  {
+    method: 'get',
+    url: '/repos/octokit-fixture-org/hello-world/contents/',
+    headers: {
+      accept: 'application/vnd.github.v3+json'
+    }
+  },
+  {
+    method: 'get',
+    url: '/repos/octokit-fixture-org/hello-world/contents/README.md',
+    headers: {
+      accept: 'application/vnd.github.v3.raw'
+    }
   }
-}
+]
