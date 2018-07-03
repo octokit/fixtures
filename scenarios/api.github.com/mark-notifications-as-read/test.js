a3200757fbc26d8b5d421c6116e2e72384866e58
Gregor <gregor@martynus.net>
2018-07-02 23:38:40 -0700

    test: adopt to mark notifications fixtures

--- scenarios/api.github.com/mark-notifications-as-read/test.js
+++ scenarios/api.github.com/mark-notifications-as-read/test.js
@@ -6,8 +6,6 @@ const fixtures = require('../../..')
 test('Labels', async (t) => {
   const mock = fixtures.mock('api.github.com/mark-notifications-as-read')
 
-  // https://developer.github.com/v3/activity/notifications/#mark-as-read
-  // Currently fails when no body is sent
   await axios({
     method: 'put',
     url: 'https://api.github.com/notifications',
@@ -16,26 +14,6 @@ test('Labels', async (t) => {
       Authorization: 'token 0000000000000000000000000000000000000001',
       'content-length': 0
     }
-  }).catch(error => {
-    if (error.response.status === 400) {
-      // GitHub API current returns an error when sending an empty body, this
-      // should get fixed soon. Once it does make sure to
-      // remove the workaround for https://github.com/octokit/rest.js/issues/694
-      return
-    }
-
-    throw error
-  }).catch(mock.explain)
-
-  await axios({
-    method: 'put',
-    url: 'https://api.github.com/notifications',
-    headers: {
-      Accept: 'application/vnd.github.v3+json',
-      Authorization: 'token 0000000000000000000000000000000000000001',
-      'Content-Type': 'application/json; charset=utf-8'
-    },
-    data: {}
   })
 
   t.doesNotThrow(mock.done.bind(mock), 'satisfies all mocks')
