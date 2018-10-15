c2cfb1243c91e554575a7b0c9aabd496bd4a1773
Gregor <gregor@martynus.net>
2018-10-15 10:34:32 -0700

    feat: errors scenario
    in preparation for https://github.com/octokit/rest.js/issues/684

---
+++ scenarios/api.github.com/errors/test.js
@@ -0,0 +1,30 @@
+const axios = require('axios')
+const { test } = require('tap')
+
+const fixtures = require('../../..')
+
+test('Errors', async (t) => {
+  t.plan(2)
+  const mock = fixtures.mock('api.github.com/errors')
+
+  try {
+    await axios({
+      method: 'post',
+      url: 'https://api.github.com/repos/octokit-fixture-org/errors/labels',
+      headers: {
+        Accept: 'application/vnd.github.v3+json',
+        Authorization: `token 0000000000000000000000000000000000000001`,
+        'Content-Type': 'application/json; charset=utf-8'
+      },
+      data: {
+        name: false
+      }
+    })
+  } catch (error) {
+    t.is(error.response.status, 422)
+  }
+
+  t.doesNotThrow(mock.done.bind(mock), 'satisfies all mocks')
+
+  t.end()
+})
