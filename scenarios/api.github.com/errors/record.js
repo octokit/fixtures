bd73a452ac1227fc66c0faef32c2345247ab4a39
Gregor <gregor@martynus.net>
2018-10-15 10:34:32 -0700

    feat: errors scenario
    in preparation for https://github.com/octokit/rest.js/issues/684

---
+++ scenarios/api.github.com/errors/record.js
@@ -0,0 +1,48 @@
+module.exports = errors
+
+const env = require('../../../lib/env')
+const getTemporaryRepository = require('../../../lib/temporary-repository')
+
+async function errors (state) {
+  let error
+  // create a temporary repository
+  const temporaryRepository = getTemporaryRepository({
+    request: state.request,
+    token: env.FIXTURES_USER_A_TOKEN_FULL_ACCESS,
+    org: 'octokit-fixture-org',
+    name: 'errors'
+  })
+
+  await temporaryRepository.create()
+
+  try {
+    // https://developer.github.com/v3/issues/labels/#create-a-label
+    // Create a label with invalid payload
+    await state.request({
+      method: 'post',
+      url: `/repos/octokit-fixture-org/${temporaryRepository.name}/labels`,
+      headers: {
+        Accept: 'application/vnd.github.v3+json',
+        Authorization: `token ${env.FIXTURES_USER_A_TOKEN_FULL_ACCESS}`
+      },
+      data: {
+        name: false
+      }
+    })
+
+      // record expected 422 error
+      .catch(error => {
+        if (error.response.status !== 422) {
+          throw error
+        }
+      })
+  } catch (_error) {
+    error = _error
+  }
+
+  await temporaryRepository.delete()
+
+  if (error) {
+    return Promise.reject(error)
+  }
+}
