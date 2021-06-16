25ef939f7b20bbf52f146b472cae8675899ee335
iczero <iczero4@gmail.com>
2021-06-16 17:21:42 -0400

    refactor(bin/record): use a for-of-loop and async function instead of `Array#reduce` with `Promise.resolve()`

--- bin/record.js
+++ bin/record.js
@@ -37,144 +37,151 @@ console.log(hasSelectedScenarios);
 const scenarios = hasSelectedScenarios
   ? selectedScenarios
   : glob.sync("scenarios/**/record.js");
-const diffs = [];
 
 console.log(`scenarios`);
 console.log(scenarios);
 
-// run scenarios one by one
-scenarios
-  .reduce(async (promise, scenarioPath) => {
-    await promise;
-    const fixtureName = scenarioPath.replace(
-      /(^scenarios\/|\/record\.js$)/g,
-      ""
-    );
-    const [domain, title] = fixtureName.split("/");
-    console.log("");
-    console.log(
-      `⏯️  ${chalk.bold(domain)}: ${humanize(title.replace(".js", ""))} ...`
-    );
+async function runScenario(scenarioPath, diffs) {
+  const fixtureName = scenarioPath.replace(
+    /(^scenarios\/|\/record\.js$)/g,
+    ""
+  );
+  const [domain, title] = fixtureName.split("/");
+  console.log("");
+  console.log(
+    `⏯️  ${chalk.bold(domain)}: ${humanize(title.replace(".js", ""))} ...`
+  );
 
-    const request = axios.create({
-      baseURL: `https://${domain}`,
-      maxRedirects: 0, // record redirects explicitly
-    });
+  const request = axios.create({
+    baseURL: `https://${domain}`,
+    maxRedirects: 0, // record redirects explicitly
+  });
 
-    // throttle writing requests
-    // https://developer.github.com/v3/guides/best-practices-for-integrators/#dealing-with-abuse-rate-limits
-    const limiter = new Bottleneck({
-      maxConcurrent: 1,
-      minTime: 3000,
-    });
-    request.interceptors.request.use((config) => {
-      if (
-        !["POST", "PATCH", "PUT", "DELETE"].includes(
-          config.method.toUpperCase()
-        )
-      ) {
-        return config;
-      }
-
-      return limiter.schedule(async () => config);
-    });
-
-    // set strict validation header, remove once stricter validations are applied
-    // to all requests: https://developer.github.com/changes/2018-11-07-strict-validation/
-    request.interceptors.request.use((config) => {
-      config.headers.Accept = `${config.headers.Accept},application/vnd.github.speedy-preview+json`;
+  // throttle writing requests
+  // https://developer.github.com/v3/guides/best-practices-for-integrators/#dealing-with-abuse-rate-limits
+  const limiter = new Bottleneck({
+    maxConcurrent: 1,
+    minTime: 3000,
+  });
+  request.interceptors.request.use((config) => {
+    if (
+      !["POST", "PATCH", "PUT", "DELETE"].includes(
+        config.method.toUpperCase()
+      )
+    ) {
       return config;
-    });
-
-    const oldNormalizedFixtures = await read(fixtureName);
-    const newRawFixtures = await recordScenario({
-      request: request,
-      scenario: (await import(`../scenarios/${fixtureName}/record.js`)).default,
-    });
-
-    const scenarioState = {
-      commitSha: {}, // map original commit sha hashes to normalized commit hashes
-      ids: {},
-    };
-
-    const newNormalizedFixtures = await Promise.all(
-      newRawFixtures
-        .map(cloneDeep)
-        .filter(hasntIgnoreHeader)
-        .map(normalize.bind(null, scenarioState))
-    );
-
-    const fixturesDiffs = diff(newNormalizedFixtures, oldNormalizedFixtures);
-    if (!fixturesDiffs) {
-      console.log("✅  Fixtures are up-to-date");
-      return;
     }
 
-    diffs.push({
-      name: fixtureName,
-      changes: fixturesDiffs,
-      newNormalizedFixtures,
-      oldNormalizedFixtures,
-      newRawFixtures,
-    });
+    return limiter.schedule(async () => config);
+  });
 
-    if (fixturesDiffs[0][0] === "-") {
-      if (doUpdate) {
-        console.log("📼  New fixtures recorded");
-        return write(fixtureName, {
-          normalized: newNormalizedFixtures,
-          raw: newRawFixtures,
-        });
-      }
-      console.log(`❌  "${fixtureName}" looks like a new fixture`);
-      return;
-    }
+  // set strict validation header, remove once stricter validations are applied
+  // to all requests: https://developer.github.com/changes/2018-11-07-strict-validation/
+  request.interceptors.request.use((config) => {
+    config.headers.Accept = `${config.headers.Accept},application/vnd.github.speedy-preview+json`;
+    return config;
+  });
 
+  const oldNormalizedFixtures = await read(fixtureName);
+  const newRawFixtures = await recordScenario({
+    request: request,
+    scenario: (await import(`../scenarios/${fixtureName}/record.js`)).default,
+  });
+
+  const scenarioState = {
+    commitSha: {}, // map original commit sha hashes to normalized commit hashes
+    ids: {},
+  };
+
+  const newNormalizedFixtures = await Promise.all(
+    newRawFixtures
+      .map(cloneDeep)
+      .filter(hasntIgnoreHeader)
+      .map(normalize.bind(null, scenarioState))
+  );
+
+  const fixturesDiffs = diff(newNormalizedFixtures, oldNormalizedFixtures);
+  if (!fixturesDiffs) {
+    console.log("✅  Fixtures are up-to-date");
+    return;
+  }
+
+  diffs.push({
+    name: fixtureName,
+    changes: fixturesDiffs,
+    newNormalizedFixtures,
+    oldNormalizedFixtures,
+    newRawFixtures,
+  });
+
+  if (fixturesDiffs[0][0] === "-") {
     if (doUpdate) {
-      console.log("📼  Fixture updates recorded");
-      return write(fixtureName, {
+      console.log("📼  New fixtures recorded");
+      await write(fixtureName, {
         normalized: newNormalizedFixtures,
         raw: newRawFixtures,
       });
-    }
-
-    console.log("❌  Fixtures are not up-to-date");
-
-    console.log(diffString(oldNormalizedFixtures, newNormalizedFixtures));
-    console.log(
-      `💁  Update fixtures with \`${chalk.bold("bin/record.js --update")}\``
-    );
-  }, Promise.resolve())
-
-  .then(() => {
-    if (diffs.length === 0) {
-      console.log("🤖  No fixture changes detected in cron job.");
       return;
     }
+    console.log(`❌  "${fixtureName}" looks like a new fixture`);
+    return;
+  }
 
-    if (doUpdate) {
-      return;
-    }
+  if (doUpdate) {
+    console.log("📼  Fixture updates recorded");
+    await write(fixtureName, {
+      normalized: newNormalizedFixtures,
+      raw: newRawFixtures,
+    });
+    return;
+  }
 
-    console.log(`${diffs.length} fixtures are out of date. Exit 1`);
-    process.exit(1);
-  })
+  console.log("❌  Fixtures are not up-to-date");
 
-  .catch((error) => {
-    if (!error.response) {
-      console.log(error);
+  console.log(diffString(oldNormalizedFixtures, newNormalizedFixtures));
+  console.log(
+    `💁  Update fixtures with \`${chalk.bold("bin/record.js --update")}\``
+  );
+}
+
+async function main() {
+  // run scenarios one by one
+  const diffs = [];
+  for (const scenarioPath of scenarios) {
+    try {
+      await runScenario(scenarioPath, diffs);
+    } catch (error) {
+      console.error(`error while running scenario ${scenarioPath}`);
+      if (!error.response) {
+        console.log(error);
+        process.exit(1);
+      }
+
+      console.log(error.toString());
+      console.log("REQUEST");
+      console.log(`${error.config.method.toUpperCase()} ${error.config.url}`);
+      console.log("RESPONSE HEADERS");
+      console.log(JSON.stringify(error.response.headers, null, 2));
+      console.log("RESPONSE BODY");
+      console.log(JSON.stringify(error.response.data, null, 2));
       process.exit(1);
     }
+  }
 
-    console.log(error.toString());
-    console.log("REQUEST");
-    console.log(`${error.config.method.toUpperCase()} ${error.config.url}`);
-    console.log("RESPONSE HEADERS");
-    console.log(JSON.stringify(error.response.headers, null, 2));
-    console.log("RESPONSE BODY");
-    console.log(JSON.stringify(error.response.data, null, 2));
-    process.exit(1);
-  });
+  if (diffs.length === 0) {
+    console.log("🤖  No fixture changes detected in cron job.");
+    return;
+  }
+
+  if (doUpdate) {
+    return;
+  }
+
+  console.log(`${diffs.length} fixtures are out of date. Exit 1`);
+  process.exit(1);
+}
+
+main();
 
 function hasntIgnoreHeader(fixture) {
   const hasIgnoreHeader = "x-octokit-fixture-ignore" in fixture.reqheaders;
